51b413931425b6930ab59da692f14747a23da051
wizo06 <14183421+wizo06@users.noreply.github.com>
2021-07-03 19:14:12 -0400

    Fixed regex to be more specific

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ const parseMessage = content => {
 
     const extractedShowNameAndEpisode = extractedFileName.replace(/^\[SubsPlease\] | \([0-9]+p\) \[[0-9A-Z]+\]\.mkv$/g, '')
     const resolution = extractedFileName.match(/\([0-9]+p\)/)[0].replace(/\(|\)|p/g, '')
-    const normalizedShowName = extractedShowNameAndEpisode.replace(/-.+$/, `- ${resolution}`)
+    const normalizedShowName = extractedShowNameAndEpisode.replace(/- [0-9]+(v|\.)[0-9]+$/, `- ${resolution}`)
 
     return {
       title: extractedFileName,
